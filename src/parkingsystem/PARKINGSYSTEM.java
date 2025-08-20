4591711f37b8ff773ef45c4cd1756ff5437e144d
arrianeeejoyyy <ajlensico@gmail.com>
2025-08-20 13:59:41 +0800

    airi

--- src/parkingsystem/PARKINGSYSTEM.java
+++ src/parkingsystem/PARKINGSYSTEM.java
@@ -1,20 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package parkingsystem;
 
-/**
- *
- * @author Airi
- */
+
 public class PARKINGSYSTEM {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
-        // TODO code application logic here
+         START lc = new START();
+       lc.setVisible(true);
+
+       
+       QN_panel sc = new QN_panel();
+       sc.setVisible(true);
     }
     
 }
